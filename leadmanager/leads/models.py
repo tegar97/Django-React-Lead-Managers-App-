1f9d35afc668fa4ed2c33aa613cf5f80c941dc31
tegar akmal <eategar@gmail.com>
2020-11-18 22:28:46 +0700

    upload to github

---
+++ leadmanager/leads/models.py
@@ -0,0 +1,7 @@
+from django.db import models
+
+class Lead(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField(max_length=100,unique=True)
+    message = models.CharField(max_length=500,blank=True)
+    create_at = models.DateTimeField(auto_now_add=True)
---
+++ leadmanager/leads/urls.py
@@ -0,0 +1,7 @@
+from rest_framework import routers
+from .api import LeadViewSet
+
+routers = routers.DefaultRouter()
+routers.register('api/leads',LeadViewSet,'leads')
+
+urlpatterns = routers.urls
